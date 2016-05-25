132dff12f65e50c73b373d1826fd235de1c5afe8
Sean Cross <xobs@kosagi.com>
2016-05-25 17:28:28 +0800

    project: fix build
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- hardware/esplanade/1.6.0/variants/code/variant.cpp
+++ hardware/esplanade/1.6.0/variants/code/variant.cpp
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 16 Chibitronics.  All right reserved.
+  Copyright (c) 2016 Chibitronics.  All right reserved.
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
