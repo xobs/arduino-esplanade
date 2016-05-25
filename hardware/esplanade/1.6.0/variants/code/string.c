7a8dfc922748839b73790807a486f9ca23da11d5
Sean Cross <xobs@kosagi.com>
2016-05-25 21:52:53 +0800

    serial and string: fix peek and tolower and toupper
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- hardware/esplanade/1.6.0/variants/code/string.c
+++ hardware/esplanade/1.6.0/variants/code/string.c
@@ -1,3 +1,7 @@
+#include "Arduino.h"
+
+long int strtol(const char *nptr, char **endptr, int base);
+void float2str(float f,char*s,unsigned int fmt);
 
 const char * strstr(const char *haystack, const char *needle) {
 
@@ -43,4 +47,22 @@ char *strrchr(const char *s, int c) {
     return (char *)end;
 
   return 0;
+}
+
+long atol(const char *nptr) {
+  return strtol(nptr, 0, 10);
+}
+
+char * dtostrf (float __val, signed char __width,
+                unsigned char __prec, char *__s) {
+  float2str(__val, __s, 0);
+  return __s;
+}
+
+int tolower(int c) {
+  return toLowerCase(c);
+}
+
+int toupper(int c) {
+  return toUpperCase(c);
 }
